b663cc145fab9abe245b16f80c017ae921a021e8
Aurelian Constantin <aurelianconstantin2020@gmail.com>
2022-07-09 19:36:14 +0300

    garbage collector - side-menu
    M2 deleted using javascript

--- forindex.js
+++ forindex.js
@@ -4,3 +4,5 @@ var elem_html = elem.innerHTML;
 var elem_new_html = elem_html.replace("(c)", "&copy;");
 console.info(elem_new_html);
 elem.innerHTML = elem_new_html;
+var m2 = document.getElementById("m2");
+m2.innerHTML = "&nbsp;";
