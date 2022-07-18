818d3562fda38348562718e940d019b735f2a4d5
Aurelian Constantin <aurelianconstantin2020@gmail.com>
2022-07-18 19:24:22 +0300

    hide all pages except the targeted one - js

--- forindex.js
+++ forindex.js
@@ -8,3 +8,28 @@ var m2 = document.getElementById("m2");
 m2.innerHTML = "&nbsp;";
 var pages = ["Home", "Skills", "Projects", "Languages"];
 console.log("first element of pages array is: " + pages[0]);
+function showHomePage() {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+function showSkillsPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+function showLanguagesPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "block";
+}
+function showProjectsPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+  document.getElementById("languages").style.display = "none";
+}
+showHomePage();
